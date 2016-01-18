1dc2565d18bdc8f2e8009b41e304b4ad46335adf
Uladzimir Vysotski <Uladzimir.Vysotski@easyjet.com>
2016-01-19 00:51:50 +0300

    initial commit

---
+++ PowerStateManagement/PowerManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PowerStateManagement
+{
+    public class PowerManager
+    {
+        const int SystemPowerInformation = 12;
+        const uint STATUS_SUCCESS = 0;
+
+        public struct SYSTEM_POWER_INFORMATION
+        {
+            public uint MaxIdlenessAllowed;
+            public uint Idleness;
+            public uint TimeRemaining;
+            public byte CoolingMode;
+        }
+
+        [DllImport("powrprof.dll", SetLastError = true)]
+        private static extern UInt32 CallNtPowerInformation(
+            int informationLevel,
+            IntPtr lpInputBuffer,
+            int nInputBufferSize,
+            out SYSTEM_POWER_INFORMATION spi,
+            int nOutputBufferSize);
+
+        public SYSTEM_POWER_INFORMATION CallPowerInfo()
+        {
+            SYSTEM_POWER_INFORMATION spi;
+            uint retval = CallNtPowerInformation(
+                SystemPowerInformation,
+                IntPtr.Zero,
+                0,
+                out spi,
+                Marshal.SizeOf(typeof(SYSTEM_POWER_INFORMATION))
+            );
+
+            if (retval == STATUS_SUCCESS)
+                return spi;
+
+            return new SYSTEM_POWER_INFORMATION();
+        }
+    }
+}
